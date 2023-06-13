87b6c6e693ed35cd1d0be1a8794914d335193aaf
NatalyOsadcha <114231206+NatalyOsadcha@users.noreply.github.com>
2023-06-14 00:55:18 +0300

    updated contacts

--- src/components/Contacts/ContactsItem.jsx
+++ src/components/Contacts/ContactsItem.jsx
@@ -38,11 +38,11 @@ const ContactsItem = ({ contact }) => {
             onChange={e => setName(e.target.value)}
           />
           <TextField
-            inputProps={{
-              pattern:"\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}",
-              title:
-                'Phone number must be digits and can contain spaces, dashes, parentheses and can start with +',
-            }}
+            // inputProps={{
+            //   pattern:"\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}",
+            //   title:
+            //     'Phone number must be digits and can contain spaces, dashes, parentheses and can start with +',
+            // }}
             id="standard-basic"
             label="number"
             variant="standard"
